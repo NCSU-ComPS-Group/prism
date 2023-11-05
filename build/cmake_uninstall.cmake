8db161944f111f5c82415701f469c0ee51ff99af
Grayson Gall <gsgall@ncsu.edu>
2023-11-05 11:56:50 -0500

    Updating gitignore

--- build/cmake_uninstall.cmake
+++
@@ -1,7 +0,0 @@
-# This script uninstalls files from an installation.
-
-if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  message(FATAL_ERROR "Installation directory not provided. Aborting uninstall.")
-endif()
-
-file(REMOVE_RECURSE "${CMAKE_INSTALL_PREFIX}")
