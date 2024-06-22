e1e98de24996ca96e1cd436881283cb4e75a0b7d
Grayson Gall <ggall713@gmail.com>
2024-06-22 15:30:03 -0600

    Trying to address other const caused build issues

--- include/rxn-cpp/Constants.h
+++ include/rxn-cpp/Constants.h
@@ -13,19 +13,19 @@ const std::string XSEC_BASED = "xsec-based";
 const std::string LATEX_OVERRIDES = "latex-overrides";
 const std::string CUSTOM_SPECIES = "custom-species";
 const std::string LUMPED_SPECIES = "lumped-species";
-const std::vector<const std::string> allowed_network_inputs = {BIB_KEY, PATH_KEY, RATE_BASED, XSEC_BASED, LATEX_OVERRIDES, CUSTOM_SPECIES, LUMPED_SPECIES};
+const std::vector<std::string> allowed_network_inputs = {BIB_KEY, PATH_KEY, RATE_BASED, XSEC_BASED, LATEX_OVERRIDES, CUSTOM_SPECIES, LUMPED_SPECIES};
 /// input keys for the custom species block
 const std::string NAME_KEY = "name";
 const std::string MASS_KEY = "mass";
-const std::vector<const std::string> allowed_custom_params = {NAME_KEY, MASS_KEY};
+const std::vector<std::string> allowed_custom_params = {NAME_KEY, MASS_KEY};
 /// input keys for lumping block
 const std::string LUMPED_KEY  = "lumped";
 const std::string ACTUAL_KEY = "actual";
-const std::vector<const std::string> allowed_lumped_params = {LUMPED_KEY, ACTUAL_KEY};
+const std::vector<std::string> allowed_lumped_params = {LUMPED_KEY, ACTUAL_KEY};
 /// inputs keys for latex overrides
 const std::string SPECIES_KEY = "species";
 const std::string LATEX_KEY =  "latex";
-const std::vector<const std::string> allowed_latex_override_params = {SPECIES_KEY, LATEX_KEY};
+const std::vector<std::string> allowed_latex_override_params = {SPECIES_KEY, LATEX_KEY};
 /// inputs that can be used by all reactions
 const std::string REACTION_KEY = "reaction";
 const std::string DELTA_EPS_E_KEY = "delta-eps-e";
@@ -36,7 +36,7 @@ const std::string REFERENCE_KEY = "references";
 const std::string FILE_KEY = "file";
 const std::string PARAM_KEY = "params";
 const unsigned int NUM_REQUIRED_ARR_PARAMS = 5;
-const std::vector<const std::string> allowed_reaction_params = {REACTION_KEY, DELTA_EPS_E_KEY, DELTA_EPS_G_KEY, ELASTIC_KEY, NOTE_KEY, REFERENCE_KEY, FILE_KEY, PARAM_KEY};
+const std::vector<std::string> allowed_reaction_params = {REACTION_KEY, DELTA_EPS_E_KEY, DELTA_EPS_G_KEY, ELASTIC_KEY, NOTE_KEY, REFERENCE_KEY, FILE_KEY, PARAM_KEY};
 
 
 const std::string ARRHENIUS = "arrhenius";
