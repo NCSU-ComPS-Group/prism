518b7ec66639af55253563a585b46171e99c7797
Grayson Gall <ggall713@gmail.com>
2024-06-22 01:55:32 -0600

    updating the issues template

--- doc/site/doxygen/html/search/classes_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['networkparser_0',['NetworkParser',['../classrxn_1_1NetworkParser.html',1,'rxn']]]
-];
--- doc/site/doxygen/html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['parsenetwork_0',['parseNetwork',['../classrxn_1_1NetworkParser.html#a30db404be00cd552434f399c2f6c65f2',1,'rxn::NetworkParser']]]
-];
