c05dc1497bfb72e308cae48548ffc4daa3c37126
cody-d10 <ctdoan@ncsu.edu>
2025-09-18 16:59:12 -0400

    Removing Singleton, issue #14

--- include/prism/NetworkParser.h
+++ include/prism/NetworkParser.h
@@ -33,8 +33,9 @@ class SpeciesSummaryWriterBase;
 class NetworkParser
 {
 public:
-  /** Getter for the singleton instance */
-  static NetworkParser & instance();
+  /** constructor was moved to public */
+  NetworkParser();
+
   /** Resets the parser to a fresh state, as if no networks have been processed */
   void clear();
   /**
@@ -169,10 +170,11 @@ public:
   void writeReactionTable(const std::string & file, TableWriterBase & writer) const;
   /**
    * Writes a summary of the species in the network to a file
-   * this method using the default summary writer provided by PRISM
+   * this method uses whatever custom summary writer that is provided to it
    * @param file the file which you want to write the species summary to
+   * @param writer the custom species writer that will be used to create the species summary
    */
-  void writeSpeciesSummary(const std::string & file);
+  void writeSpeciesSummary(const std::string & file) const;
   /**
    * Writes a summary of the species in the network to a file
    * this method uses whatever custom summary writer that is provided to it
@@ -182,17 +184,8 @@ public:
   void writeSpeciesSummary(const std::string & file, SpeciesSummaryWriterBase & writer) const;
 
 private:
-  /** private constructor because only this class can create itself */
-  NetworkParser();
-  /** delete the copy constructor to make this a singleton */
-  NetworkParser(const NetworkParser &) = delete;
-  /** delete the copy assignment to make this a singleton */
-  NetworkParser & operator=(const NetworkParser &) = delete;
   SpeciesFactory & _factory;
   BibTexHelper & _bib_helper;
-  /// Private instance of the singleton,
-  /// this is the only instance of this class that will ever exist
-  static NetworkParser * _instance;
   /// wether or not the parser encountered any errors during parsing
   bool _network_has_errors;
   /// wether or not the parser encountered any issues with the bib files while parsing
